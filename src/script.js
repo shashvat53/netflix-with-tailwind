341d98ce2d849e14927bea9288d4d130d43de8d1
shashvat sen <shashvatsen00@gmail.com>
2023-09-03 02:14:01 +0530

    second commit

--- src/script.js
+++ src/script.js
@@ -1,6 +1,25 @@
-const navbar = document.getElementById("navbar");
-const menu = document.getElementById("menu");
-navbar.addEventListener("click", () => {
-  //   alert("Hello, world");
-  menu.classList.remove("hidden");
+const que = document.querySelectorAll(".faq");
+const plus = document.querySelector(".plus");
+console.log(plus);
+
+que.forEach((e, index) => {
+  e.addEventListener("click", () => {
+    e.nextElementSibling.classList.toggle("hidden");
+    e.children[1].classList.toggle("rotate-45");
+    // plus.innerText = "-";
+    // ans.classList.toggle("hidden");
+    que.forEach((v, i) => {
+      if (i != index) {
+        v.nextElementSibling.classList.add("hidden");
+        v.children[1].classList.remove("rotate-45");
+      }
+    });
+  });
 });
+
+// que.addEventListener("click", (e) => {
+//   ans.classList.toggle("hidden");
+// });
+
+// alert("hffii");
+// console.log("Jai shree ram");
